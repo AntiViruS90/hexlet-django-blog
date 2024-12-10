5969343d30b4316182d7833173f76550e1d0bfa0
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2024-12-10 17:09:28 +0300

    Add template for app article

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def index(req):
-    return HttpResponse('article')
+    return render(req, 'articles.html')
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -7,4 +7,9 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'about.html')
+    tags = ['обучение', 'программирование', 'python', 'ООП']
+    return render(
+        request,
+        'about.html',
+        context={'tags': tags}
+    )
